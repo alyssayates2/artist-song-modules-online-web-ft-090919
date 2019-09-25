380bd02af9e7bfc08bfc09a1d38482a38fcba16c
Alyssa Yates <bosslys@icloud.com>
2019-09-25 20:01:35 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,15 +2,4 @@ module Findable
 
   attr_accessor :name
 
-  # def initialize(name)
-  #   @name = name
-  # end
-
-  # def self.all
-  #   @@artists
-  # end
-  #
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
 end
