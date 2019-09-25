83108ed1c7cb48f1f90a21c2a3304b300faa47cb
Alyssa Yates <bosslys@icloud.com>
2019-09-25 19:55:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,14 +11,14 @@ class Artist
   @@artists = []
 
   def initialize
-    @name = name
+    # @name = name
     @@artists << self
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
